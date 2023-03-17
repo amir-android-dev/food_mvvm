5d7e7c9b968cad0c1bcebff5567414ca6b30d11f
ema <amir-hosseini@gmx.at>
2023-03-17 20:23:12 +0100

    checking with gitlab manage remote

--- app/src/main/java/com/challenge3/food_mvvm/ui/list/FoodsListFragment.kt
+++ app/src/main/java/com/challenge3/food_mvvm/ui/list/FoodsListFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import coil.load
-import com.challenge3.food_mvvm.R
 import com.challenge3.food_mvvm.databinding.FragmentFoodsListBinding
 import com.challenge3.food_mvvm.viewmodel.FoodsListViewModel
 import dagger.hilt.android.AndroidEntryPoint
